3b744d24342cf0cb20c2fe5e8e09660b48eae28e
Kyle Weintraub <Weintraub.Kyle@gmail.com>
2018-01-02 12:22:24 -0700

    style update

--- app.js
+++ app.js
@@ -1,45 +1,37 @@
-var players = []
-
 var myFirebaseRef = new Firebase("https://g70-catan-leaderboard.firebaseio.com/")
 var playersRef = new Firebase("https://g70-catan-leaderboard.firebaseio.com/players")
+
+var players = []
 myFirebaseRef.on("value", function(snapshot) {
   snapshot.val().players.forEach(function(e){
     players.push(e)
   })
 })
 
-
-var ranked = rankEm(players)
-
-// playersRef.child(4).child("name").set("OG Settler")
-// playersRef.update({id: 7, name: "Ignacio", wins: 2, quote: "You kill my father, prepare to die"})
-
-// myFirebaseRef.set({players: {"4" : {name: "adam da OG"}}});
-
-
-// function newWinner(player, string) {
-//   playersRef.push({id: "new", name: player, wins: 1, quote: string})
-// }
-
 function rankEm(players){
-  return players.sort(function(a,b){
-  	return a.wins - b.wins
+  var wins = players.sort(function(a,b){
+    return a.id - b.id
+  })
+  return wins.sort(function(a,b){
+    return a.wins - b.wins
   })
 }
 
 
-
 const board = document.querySelector('#board')
-
 function appendPlayers(playerArray){
+  console.log("Hard Refresh if nothing shows up.");
   for (var i = 0; i < playerArray.length; i++){
     var entry = document.createElement('tr')
     var name = document.createElement('td')
     name.textContent = playerArray[i].name
+    name.className = "player-name"
     var wins = document.createElement('td')
     wins.textContent = playerArray[i].wins
+    wins.className = "win-count"
     var quote = document.createElement('td')
-    quote.textContent = playerArray[i].quote
+    quote.textContent = `"${playerArray[i].quote}"`
+    quote.className = "quotation"
     entry.appendChild(wins)
     entry.appendChild(name)
     entry.appendChild(quote)
@@ -50,4 +42,26 @@ function appendPlayers(playerArray){
 setTimeout(function(){
   rankEm(players).reverse()
   appendPlayers(players)
-}, 2000)
+}, 1000)
+
+
+// const form = document.querySelector('form')
+// form.addEventListener('submit', function(e){
+//   e.preventDefault()
+//   var formObj = new FormData(e.target)
+//   let winner = formObj.get("playerName")
+//   // addWin(winner)
+// })
+
+// function addWin(string){
+//   if (string === )
+// }
+// playersRef.child(4).child("name").set("OG Settler")
+// playersRef.update({id: 7, name: "Ignacio", wins: 2, quote: "You kill my father, prepare to die"})
+
+// myFirebaseRef.set({players: {"4" : {name: "adam da OG"}}});
+
+
+// function newWinner(player, string) {
+//   playersRef.push({id: "new", name: player, wins: 1, quote: string})
+// }
